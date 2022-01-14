6db6cb31dfde7fe66614e7adb76d5429de44ae1d
Guilherme Illescas <guilhermeillescas@Guilhermes-MacBook-Pro.local>
2022-01-14 17:37:25 -0300

    Configure global themes of stylde components

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+
+import * as Styles from './styles';
+
+const Dashboard = (): ReactElement => {
+  return (
+    <Styles.Container>
+
+    </Styles.Container>
+  );
+};
+
+export default Dashboard
